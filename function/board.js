ed80decb004ea8d71abed16224f94b20c185c7f2
EstebanCampos <campos.esteban@gmail.com>
2015-05-17 20:08:32 -0400

    Se juega solo presionando tablero

--- function/board.js
+++ function/board.js
@@ -1,26 +1,29 @@
-//Se ejecuta primero
+//Declaro dependencias
 var socket = io();
 
-$(document).ready(function(){  
 
-    $('#btn-send').click(function (event) {
-        var fila = $("#fila").val();
-        var col = $("#col").val();
-        var jugador= $("#jugador").val();
+//Declaro variables globales
+var gtipoJugador='',
+    nickUsuario='',
+    tablero=[];
 
-       
+$(document).ready(function(){
 
-        if(jugador==='Equis')
-            jugador= 'X';
-        else
-            jugador='O';
-        
+
+
+    $('.cuadro').click(function (event) {
+
+        var partes= (this.id).split('-'),
+            fila = partes[0].replace('fila',''),
+            col = partes[1].replace('col',''),
+            jugador= gtipoJugador;
+
         socket.emit('jugada', {fila: fila,
                                col : col,
                                jugador:jugador} );
-       
+
     });
-    
+
      socket.on('jugada activa', function(msg){
           //Dibujo un circulo en la posicion indicada: hay que descifrar msg-> fila y columna
          var fila = msg.fila,
@@ -28,10 +31,10 @@ $(document).ready(function(){
              jugador= msg.jugador;
 
          if(jugador==='X')
-            $('#row'+fila + '-col'+col).html('<img style="height: 120px;width:120px; "src="../img/ink-x.png" />');
+            $('#fila'+fila + '-col'+col).html('<img style="height: 120px;width:120px; "src="../img/ink-x.png" />');
          else
-            $('#row'+fila + '-col'+col).html('<img style="height: 120px;width:120px; "src="../img/ink-circle.png" />');
-      
+            $('#fila'+fila + '-col'+col).html('<img style="height: 120px;width:120px; "src="../img/ink-circle.png" />');
+
     });
 
 
@@ -46,16 +49,13 @@ $(document).ready(function(){
             document.getElementById("txt-resultado").innerHTML = 'Gano jugador O';
             alert('Gano O');
          }
-      
-    }); 
+
+    });
 
     socket.on('Designar', function(msg){
         document.getElementById("txt-resultado").innerHTML = msg.texto;
 
-        if(msg.jugador==='X')
-            $('#jugador').val('Equis');
-        else
-            $('#jugador').val('Circulo');
+        gtipoJugador= msg.jugador;
 
     });
 
@@ -65,7 +65,7 @@ $(document).ready(function(){
 
     socket.on('Msje_Personal', function(msg){
         document.getElementById("txt-resultado").innerHTML = msg.texto;
-       
+
     });
 
 
@@ -78,14 +78,14 @@ $(document).ready(function(){
 //Esta funcion es para enviar mensajes a servidor
 function send(fila, col, jugador){
 
-    var urlTweet = 'http://localhost:8080/colocarJugada/' + fila + '/' + col + '/' + jugador;  
+    var urlTweet = 'http://localhost:8080/colocarJugada/' + fila + '/' + col + '/' + jugador;
 
-    $.ajax({ url: urlTweet, type: 'GET', dataType: "json", 
+    $.ajax({ url: urlTweet, type: 'GET', dataType: "json",
     success: function(resultData) {
-       
-        //Escribo mensaje en la pantalla 
+
+        //Escribo mensaje en la pantalla
         document.getElementById("txt-resultado").innerHTML = resultData;
-   
+
     },
     error: function(xhr, status, error){
         console.log(xhr);
